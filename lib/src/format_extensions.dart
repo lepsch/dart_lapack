134cc80f27921c213bc72f47bb0ae96d30f03404
Guilherme Lepsch <lepsch@gmail.com>
2024-03-03 01:48:11 +0000

    Add lists to format

--- lib/src/format_extensions.dart
+++ lib/src/format_extensions.dart
@@ -3,24 +3,28 @@ import 'dart:math';
 import 'package:lapack/src/matrix.dart';
 
 extension IntFormatExtension on int {
-  String get i1 => toString().padLeft(1).w(1);
-  String get i2 => toString().padLeft(2).w(2);
-  String get i3 => toString().padLeft(3).w(3);
-  String get i4 => toString().padLeft(4).w(4);
-  String get i5 => toString().padLeft(5).w(5);
-  String get i6 => toString().padLeft(6).w(6);
-  String get i7 => toString().padLeft(7).w(7);
-  String get i8 => toString().padLeft(8).w(8);
-  String get i12 => toString().padLeft(12).w(12);
-  String get i15 => toString().padLeft(15).w(15);
-  String get i36 => toString().padLeft(36).w(36);
+  String i(int w) => toString().padLeft(w).w(w);
+
+  String get i1 => i(1);
+  String get i2 => i(2);
+  String get i3 => i(3);
+  String get i4 => i(4);
+  String get i5 => i(5);
+  String get i6 => i(6);
+  String get i7 => i(7);
+  String get i8 => i(8);
+  String get i12 => i(12);
+  String get i15 => i(15);
+  String get i36 => i(36);
 }
 
 extension DoubleFormatExtension on double {
   // fixed-point
   String f(int w, [int d = 0]) => toStringAsFixed(d).padLeft(w).w(w);
+  String get f4_1 => f(4, 1);
   String get f4_2 => f(4, 2);
   String get f5_0 => f(5, 0);
+  String get f6_1 => f(6, 1);
   String get f8_2 => f(8, 2);
   String get f12_2 => f(12, 2);
   String get f12_3 => f(12, 3);
@@ -38,6 +42,7 @@ extension DoubleFormatExtension on double {
         .padLeft(w);
   }
 
+  String get d9_1 => d(9, 1);
   String get d10_3 => d(10, 3);
   String get d12_3 => d(12, 3);
   String get d12_4 => d(12, 4);
@@ -87,6 +92,7 @@ extension DoubleFormatExtension on double {
   String get g11_4 => g(11, 4);
   String get g13_6 => g(13, 6);
   String get g16_6 => g(16, 6);
+  String get g18_6 => g(18, 6);
 
   String get p => sp;
   String get sp => this >= 0 ? '+' : '-';
@@ -94,32 +100,54 @@ extension DoubleFormatExtension on double {
 }
 
 extension StringFormatExtension on String {
-  String get a1 => padLeft(1).substring(0, 1);
-  String get a2 => padLeft(2).substring(0, 2);
-  String get a3 => padLeft(3).substring(0, 3);
-  String get a4 => padLeft(4).substring(0, 4);
-  String get a6 => padLeft(6).substring(0, 6);
-  String get a7 => padLeft(7).substring(0, 7);
-  String get a10 => padLeft(10).substring(0, 10);
-  String get a15 => padLeft(15).substring(0, 15);
-  String get a17 => padLeft(17).substring(0, 17);
-  String get a79 => padLeft(79).substring(0, 79);
+  String a(int w) => padLeft(w).substring(0, w);
+
+  String get a1 => a(1);
+  String get a2 => a(2);
+  String get a3 => a(3);
+  String get a4 => a(4);
+  String get a6 => a(6);
+  String get a7 => a(7);
+  String get a10 => a(10);
+  String get a15 => a(15);
+  String get a17 => a(17);
+  String get a79 => a(79);
 
   String w(int w) => length > w ? '*' * w : this;
 }
 
+extension BoolFormatExtension on bool {
+  String get l1 => (this ? 'T' : 'F');
+  String get l2 => (this ? ' T' : ' F');
+}
+
+extension StringIterableFormatExtension on Iterable<String> {
+  String a(int w, [int? len, String separator = '']) =>
+      (len != null ? take(len) : this).map((s) => s.a(w)).join(separator);
+
+  String a1([int? len, String separator = '']) => a(1, len, separator);
+}
+
+extension IntIterableFormatExtension on Iterable<int> {
+  String i(int w, [int? len, String separator = '']) =>
+      (len != null ? take(len) : this).map((n) => n.i(w)).join(separator);
+
+  String i3([int? len, String separator = '']) => i(3, len, separator);
+}
+
 extension IntArrayFormatExtension on Array<int> {
-  String i4(int x, [String separator = '']) =>
-      [for (var i = 1; i <= x; i++) this[i]].map((n) => n.i4).join(separator);
-  String i5(int x, [String separator = '']) =>
-      [for (var i = 1; i <= x; i++) this[i]].map((n) => n.i5).join(separator);
-  String i6(int x, [String separator = '']) =>
-      [for (var i = 1; i <= x; i++) this[i]].map((n) => n.i6).join(separator);
-  String i8(int x, [String separator = '']) =>
-      [for (var i = 1; i <= x; i++) this[i]].map((n) => n.i8).join(separator);
+  String i(int w, int len, [String separator = '']) =>
+      [for (var i = 1; i <= len; i++) this[i]].i(w, len);
+
+  String i4(int len, [String separator = '']) => i(4, len, separator);
+  String i5(int len, [String separator = '']) => i(5, len, separator);
+  String i6(int len, [String separator = '']) => i(6, len, separator);
+  String i8(int len, [String separator = '']) => i(8, len, separator);
 }
 
 extension DoubleArrayFormatExtension on Array<double> {
   String d12_3(int x) =>
       [for (var i = 1; i <= x; i++) this[i]].map((n) => n.d12_3).join();
+  String f6_1(int x) =>
+      [for (var i = 1; i <= x; i++) this[i]].map((n) => n.f6_1).join();
 }
